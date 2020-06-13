471a4f5acb3a5b2f576a66376e987d625840fb36
Matthew Bly <skyguard14@iCloud.com>
2020-06-12 21:08:09 -0700

    Module 8 PA Code Tested and working

--- Prob-1/Prob-1.py
+++ Prob-1/Prob-1.py
@@ -3,3 +3,60 @@
 #     Prob-1.py
 
 # Your code below
+class Car:
+    def __init__(self, make, model, year):
+        self._make = make
+        self._model = model
+        self._year = year
+
+    def set_make(self, make):
+        self._make = make
+
+    def get_make(self):
+        return self._make
+
+    def set_model (self, model):
+        self._model = model
+
+    def get_model(self):
+        return self._model
+    
+    def set_year(self, year):
+        self._year = year
+
+    def get_year(self):
+        return self._year
+
+def TestCar():
+    carLot = []
+    carLot.append(Car("lamborghini:","Aventador:","2001"))
+    carLot.append(Car('Ford:', "Mustang:", "2008"))
+    carLot.append(Car('chevy:', "Bolt:", "2005"))
+    carLot.append(Car('Honda:', "Accord:", "2011"))
+    carLot.append(Car('Ford:', "Focus:", "2009"))
+    
+    for element in carLot:
+        print(element.get_make(),element.get_model(),element.get_year())
+
+    for car in carLot:
+        print()
+        make = input("enter make:")
+        car.set_make(make)
+        
+        model = input("enter model:")
+        car.set_model(model)
+     
+        year = input("enter year:")
+        car.set_year(year)
+    
+    for element in carLot:
+        print(element.get_make(),element.get_model(),element.get_year())
+        
+    
+
+if __name__ == "__main__":
+    TestCar()
+    
+
+
+
--- Prob-2/Prob-2.py
+++ Prob-2/Prob-2.py
@@ -3,3 +3,58 @@
 #     Prob-2.py
 
 # Your code below
+
+class Student:
+    def __init__(self, name, IDnumber, major, GPA):
+        self._name = name
+        self._IDnumber = IDnumber
+        self._major = major
+        self._GPA = GPA
+    
+    def set_name(self, name):
+        self._name = name
+
+    def get_name(self):
+        return self._name
+
+    def set_IDnumber(self, IDnumber):
+        self._IDnumber = IDnumber
+
+    def get_IDnumber(self):
+        return self._IDnumber
+    
+    def set_major(self, major):
+        self._major = major
+
+    def get_major(self):
+        return self._major
+
+    def set_GPA(self, GPA):
+        self._GPA = GPA
+    
+    def get_GPA(self):
+        return self._GPA
+
+def TestStudents():
+    print()
+    studentList = []
+    studentList.append(Student('Joe Bella', "\t" "9933", "\t" "Web Development", "3.8"))
+    studentList.append(Student('Tucker Blank', "\t" "3399", "\t" "Nursing", "\t" "3.0"))
+    studentList.append(Student('Gayle Ujifusa ', "\t" "1011", "\t" "Baking", "\t" "\t" "2.8"))
+    studentList.append(Student('Edna Anker', "\t" "9875", "\t" "Medical Office", "\t" "3.0"))
+  
+    
+    
+
+
+    for student in studentList:
+        print(student.get_name(), student.get_IDnumber(), student.get_major(), student.get_GPA())
+        
+        
+
+
+
+if __name__ == "__main__":
+    TestStudents()
+    print()
+        
